ca611af98ba29bae54266fd4d19db20ce29b176a
vlyd04 <131730271+vlyd04@users.noreply.github.com>
2024-04-30 21:25:24 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+// import image from "./assest/Journey .gif"
+const Home = () => {
+  return (
+    <> 
+    <div className="cont">
+      {/* <img src={image} alt="world" className="im"/> */}
+    </div>
+    
+    </>
+   
+  );
+};
+export default Home;
+
